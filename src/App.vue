06d0602330812d7e5f4cb6988117f572c2a4279c
Chris Cromer <chris@cromer.cl>
2023-04-25 22:49:05 -0400

    add genre and description

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,17 @@ import { items } from "./movies.json";
 
 <template>
   <!-- This is where your template goes	-->
-  <div class="flex justify-center w-full py-4">
+  <div class="flex justify-center w-full py-4 px-12">
     <div class="grid grid-cols-4 gap-8">
-      <div v-for="item in items">
-        <img class="w-72 h-96" :src="item.image">
+      <div class="flex flex-col m-4" v-for="item in items">
+        <img class="w-full h-96" :src="item.image">
+        <div class="flex-1 p-4 bg-white">
+          <div class="font-bold">{{ item.name }}</div>
+          <div class="py-2">
+            <span class="bg-blue-700 text-white m-1 p-1" v-for="genre in item.genres">{{ genre }}</span>
+          </div>
+          <div>{{ item.description }}</div>
+        </div>
       </div>
     </div>
   </div>
